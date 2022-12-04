567395f0840df7bfc02900e0b8509ac188174532
Gessio Mori <gessiomorin@gmail.com>
2022-12-04 10:57:18 -0300

    4th day: a and b are almost the same =)

---
+++ 4/b.ts
@@ -0,0 +1,21 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const pairs = input.split("\n").map((pair) =>
+  pair.split(",").map((list) => ({
+    min: Number(list.split("-")[0]),
+    max: Number(list.split("-")[1]),
+  }))
+);
+
+let numOfPairsThatOverlap = 0;
+
+for (const pair of pairs) {
+  if (
+    (pair[0].min >= pair[1].min && pair[0].min <= pair[1].max) ||
+    (pair[1].min >= pair[0].min && pair[1].min <= pair[0].max)
+  ) {
+    numOfPairsThatOverlap++;
+  }
+}
+
+console.log(numOfPairsThatOverlap);
